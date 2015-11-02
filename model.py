83190c39040fe8be4daabaeb5eb376f885f5b485
Kara DeWalt <karadewalt@Kara-DeWalts-MacBook.local>
2015-11-02 14:33:30 -0800

    Working model and server. Stores most recent user in the session with their calculated VDOT. Needs login to access existing users.

--- model.py
+++ model.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import calculator
+from datetime import timedelta
 
 app = Flask(__name__)
 
@@ -18,23 +20,83 @@ class User(db.Model):
 
     user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     email = db.Column(db.String(50), unique=True)
-    # distance is in meters
-    distance = db.Column(db.Integer, nullable=False)
-    # time is in minutes
-    time = db.Column(db.Integer, nullable=False)
-    VDOT = db.Column(db.Integer, nullable=False)
-    weekly_mileage = db.Column(db.Integer)
+    # distance is in meters -TODO!! will be removed when Race is routes are in place
+    # distance = db.Column(db.Integer, nullable=False)
+    # time is in minutes - TODO!! will be removed when Race is routes are in place
+    # time = db.Column(db.Integer, nullable=False)
+    # VDOT = db.Column(db.Integer, nullable=False)
+    weekly_mileage = db.Column(db.Integer, nullable=True)
 
     def greet(self):
         """Greet using email"""
 
         return "Hello, {}".format(self.email)
 
+
     def __repr__(self):
         """Provide helpful representation when printed"""
 
-        string = "<User id = {} VDOT = {} Max Weekly Milage = {}>"
+        string = "<User id = {} VDOT = {} Max Weekly Mileage = {}>"
         return string.format(self.user_id, self.VDOT, self.weekly_mileage)
+
+
+class Race(db.Model):
+    """Store users races to compute VDOT from"""
+
+    __tablename__ = "races"
+
+    race_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"), nullable=False)
+    # distance in meters
+    distance = db.Column(db.Integer, nullable=False)
+    # time in minutes
+    time = db.Column(db.Integer, nullable=False)
+
+    user = db.relationship("User", backref=db.backref("races", order_by=race_id))
+
+    def VDOT(self):
+        """Return user VDOT"""
+
+        percent_VO2 = calculator.get_percent_VO2max(self.time)
+        vel = calculator.velocity(self.distance, self.time)
+        race_VO2 = calculator.get_VO2_from_velocity(vel)
+        VDOT = race_VO2 / percent_VO2
+
+        return VDOT
+
+    def __repr__(self):
+        """Provide helpful representation when printed"""
+
+        string = "<Race id: {}, User id: {}, distance: {}, time: {}>"
+        return string.format(self.race_id, self.user_id, self.distance, self.time)
+
+
+class Pace(object):
+#     """Store paces E, M, T as percentages"""
+
+    PACE_DICT = {
+        "easy": (0.55, 0.65, 0.74),
+        "marathon": (0.75, 0.79, 0.84),
+        "tempo": (0.83, 0.86, 0.89)
+    }
+
+    def __init__(self, VDOT, intensity):
+        self.VDOT = VDOT
+        self.intensity = intensity
+
+    def pace_range(self):
+        intensity_tuple = PACE_DICT[self.intensity]
+        p_range = []
+        for t in intensity_tuple:
+            percent_VDOT = self.VDOT * t
+            velocity = calculator.get_velocity_from_VO2(percent_VDOT)
+            miles_per_min = velocity / 1609.34
+            minutes_per_mile = timedelta(minutes=(1/miles_per_min))
+            p_range.append(miles_per_min)
+        return p_range
+
+
+
 
 ################################################################################
 # Helper Functions
@@ -42,11 +104,11 @@ class User(db.Model):
 
 def connect_to_db(app):
     """Connect to the database."""
-
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///model.db'
     app.config['SQLALCHEMY-ECHO'] = True
     db.app = app
     db.init_app(app)
+    db.create_all()
 
 connect_to_db(app)
 
--- server.py
+++ server.py
@@ -6,7 +6,7 @@ from flask import Flask, render_template, redirect, request, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from datetime import timedelta
 import calculator
-from model import connect_to_db, db, User
+from model import connect_to_db, db, User, Race, Pace
 
 app = Flask(__name__)
 
@@ -24,7 +24,7 @@ def index():
     return render_template("home.html")
 
 
- #     return render_template("home.html", blah=blah)
+ # return render_template("home.html", blah=blah)
 
 @app.route("/calculate-VDOT", methods=["POST"])
 def create_table():
@@ -33,23 +33,34 @@ def create_table():
     ss = request.form.get("seconds")
     units = request.form.get("units")
     distance = float(request.form.get("distance"))
-    mileage = float(request.form.get("mileage"))
+    email = request.form.get("email")
 
     # have function in calculator.py (with doctest) would it be clear to call .calculator.function()?
     time = float(mm) + (float(hr) * 60) + (float(ss) / 60)
 
+    distance_in_meters = calculator.convert_distance_to_meters(distance, units)
 
-    VDOT = calculator.user_VDOT(distance, units, time)
-    print VDOT
+    # should exist on Race class, remove VDOT variable
+    # VDOT = calculator.user_VDOT(distance, units, time)
+    # print VDOT
     # session["VDOT"] = VDOT
-
-    new_user = User(distance=distance, time=time, VDOT=VDOT, weekly_mileage=mileage)
+    # -------TODO--------
+        # send distance and time to races tabel under user email/id
+    new_user = User(email=email)
     db.session.add(new_user)
     db.session.commit()
 
-    return render_template("form-submit.html")
+    user_obj = User.query.filter(User.email == email).first()
+    user_id = user_obj.user_id
+    session["user_id"] = user_id
 
+    new_race = Race(user_id=session["user_id"], distance=distance_in_meters, time=time)
+    db.session.add(new_race)
+    db.session.commit()
 
+    session["VDOT"] = new_race.VDOT()
+
+    return render_template("form-submit.html", VDOT=session["VDOT"])
 
 
 if __name__ == "__main__":
